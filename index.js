48e4e3bb8e5c51b252ac5cee79a06daad5ab91c2
StephhyL <Stephanie Li>
2021-11-05 12:57:31 -0400

    added newTask object

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
-
-// Create a new task by adding to the arrays
-// Create a description of the new task and add it into the taskDescription array
-// A new task will be created as incomplete
-function newTask(title, description) {
-  taskTitles.push(title);
-  taskDescriptions.push(description);
-  taskComplete.push(false);
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
 
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+// creates a new function that takes in title and description, and returns a task object
+
+const newTask = (title, description) => {
+  const task = {
+    title,  // title: title,
+    description, // description: description
+    complete: false
+  }
+  return task;
 }
 
 
@@ -31,11 +17,32 @@ function logTaskState(taskIndex) {
 
 
 
-// DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+
+
+
+
+
+
+// // Print the state of a task to the console in a nice readable way
+// function logTaskState(taskIndex) {
+//   const title = taskTitles[taskIndex];
+//   const complete = taskComplete[taskIndex];
+//   console.log(`${title} has${complete ? " " : " not "}been completed`);
+// }
+
+
+
+
+
+
+
+// // DRIVER CODE BELOW
+
+// newTask("Clean Cat Litter"); // task 0
+// newTask("Do Laundry"); // task 1
+
+// logTaskState(0); // Clean Cat Litter has not been completed
+// completeTask(0);
+// logTaskState(0); // Clean Cat Litter has been completed

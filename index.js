28c317c090a0155a6e33fdc87ef977b97fbbc5c3
StephhyL <Stephanie Li>
2021-11-05 12:54:45 -0400

    step 1 added new array for taskDescription

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
+const taskDescriptions = [];
 
 // Create a new task by adding to the arrays
+// Create a description of the new task and add it into the taskDescription array
 // A new task will be created as incomplete
-function newTask(title) {
+function newTask(title, description) {
   taskTitles.push(title);
+  taskDescriptions.push(description);
   taskComplete.push(false);
 }
 
@@ -14,12 +17,19 @@ function completeTask(taskIndex) {
   taskComplete[taskIndex] = true;
 }
 
+
 // Print the state of a task to the console in a nice readable way
 function logTaskState(taskIndex) {
   const title = taskTitles[taskIndex];
   const complete = taskComplete[taskIndex];
   console.log(`${title} has${complete ? " " : " not "}been completed`);
 }
+
+
+
+
+
+
 
 // DRIVER CODE BELOW
 

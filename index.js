051fc84ca59b515fb328451060141fabbb68fad3
StephhyL <Stephanie Li>
2021-11-05 13:06:04 -0400

    added logstate and markCompleted as methods into the task object

--- index.js
+++ index.js
@@ -7,11 +7,29 @@ const newTask = (title, description) => {
     title,  // title: title,
     description, // description: description
     complete: false
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    }
+
+    markCompleted: function() {
+      this.compelte = true;
+    }
+
+
+
   }
   return task;
-}
+};
 
 
+
+const task1 = newTask("Clean Cat Litter", "Take all the pooooooop out of the litter box");
+const task2 = newTask("Do Laundry", "ohhhh noooooooo");
+const tasks = [task1, task2];
+
+
+console.log(tasks);
 
 
 
